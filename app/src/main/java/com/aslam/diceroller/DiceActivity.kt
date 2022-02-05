366f39da86d2848e6b95f3de60a62caa1c6475c7
ShaikAslam1 <aslamshaik281@gmail.com>
2022-02-05 12:11:44 +0530

    added dice images and test cases

--- app/src/main/java/com/aslam/diceroller/DiceActivity.kt
+++ app/src/main/java/com/aslam/diceroller/DiceActivity.kt
@@ -1,6 +1,9 @@
 package com.aslam.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class DiceActivity : AppCompatActivity() {
@@ -9,14 +12,33 @@ class DiceActivity : AppCompatActivity() {
         setContentView(R.layout.activity_dice)
 
         val actionBar = supportActionBar
-
 //        if(actionBar != null) {
 //            actionBar.title = "Dice graphical game"
 //        }
-
         actionBar!!.title = "Dice graphical game"
-
         actionBar.setDisplayHomeAsUpEnabled(true)
 
+//      val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceBtn: Button = findViewById(R.id.button4)
+
+        diceBtn.setOnClickListener { rollDice() }
+
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when(diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+        Toast.makeText(this, "Dice Rolled: $diceRoll", Toast.LENGTH_SHORT).show()
     }
 }
